49d3c973f475755658aef492a4fdd591fc3b6538
Gurnam Sunny <sunny@sunache.com>
2018-07-17 20:20:23 -0700

    changes to routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,107 @@
-var data = [
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [{
+        name: "Aarv",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwihqLCe16fcAhUJwFQKHa2pC0YQjRx6BAgBEAU&url=https%3A%2F%2Ftimesofindia.indiatimes.com%2Fentertainment%2Fhindi%2Fbollywood%2Fphoto-features%2Fakshay-kumar-what-sets-him-apart-from-the-khans%2FAkshay-Kumar-refuses-to-leave-the-sets-of-Kesari-after-injuring-himself%2Fphotostory%2F63825818.cms&psig=AOvVaw1BwIUNRTUHAfeVC2Y0FQYY&ust=1531970030569521",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
     {
-        name: "Sunny",
-        photoLink: "www.google.com",
-        answers: [],
-  },
+        name: "Paul",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwji8PG_16fcAhUrhlQKHS8PBkAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.artstation.com%2Fakshay&psig=AOvVaw1BwIUNRTUHAfeVC2Y0FQYY&ust=1531970030569521",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Will",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjqh6bS16fcAhUKKHwKHdAhAHEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.tes.com%2Flessons%2Fu3ziQihHqXEBGw%2Ftheatre-research&psig=AOvVaw3sW9OrUYiJfXSKJV01DgAP&ust=1531970133932030",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Morgan",
+        photo: "https://d1u4oo4rb13yy8.cloudfront.net/article/bwmsggxwqh-1527179279.jpg",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Lou Ritter",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Hrithik",
+        photo: "https://media2.mensxp.com/media/content/2018/Jan/hrithik-roshan-has-been-ranked-world-rsquo-s-most-handsome-actor1400-1516009828_1100x513.jpg",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
 ];
 
-
-module.exports = data;
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,25 +1,65 @@
 
-var tableData = require("../data/friends");
-
-
+var friends = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
+    // API GET Requests
 
-    app.get("/api", function (req, res) {
-        res.json(data);
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
     });
 
+    // API POST Requests
+    // ---------------------------------------------------------------------------
 
+    app.post("/api/friends", function (req, res) {
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
 
-    app.post("/", function (req, res) {
+        // Here we take the result of the user"s survey POST and parse it.
+        var userData = req.body;
+        var userScores = userData.scores;
 
-            data.push(req.body);
-            res.json(true);
-    
+        // This variable will calculate the difference between the user"s scores and the scores of
+        // each user in the database
+        var totalDifference;
+
+        // Here we loop through all the friend possibilities in the database.
+        for (var i = 0; i < friends.length; i++) {
+            var currentFriend = friends[i];
+            totalDifference = 0;
+
+            console.log(currentFriend.name);
+
+            // We then loop through all the scores of each friend
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+
+                // We calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+
+            // If the sum of differences is less then the differences of the current "best match"
+            if (totalDifference <= bestMatch.friendDifference) {
+                // Reset the bestMatch to be the new friend.
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+
+        // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+        // the database will always return that the user is the user's best friend).
+        friends.push(userData);
+
+        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+        res.json(bestMatch);
     });
-
 };
